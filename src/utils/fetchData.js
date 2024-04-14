05e62b87a280f5f8f98ef339e30a7615200869d5
unknown <jamilafzal255@gmail.com>
2024-04-15 03:32:04 +0500

    under work

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ff96d73bd1msh61b58290e887b4dp10e5ecjsn461a025d8b50',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+
+    return data;
+};
