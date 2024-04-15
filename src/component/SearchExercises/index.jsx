eeeecea441fc640defbee921b99df0d46cfbfd07
unknown <jamilafzal255@gmail.com>
2024-04-16 01:50:51 +0500

    add  search bar

--- src/component/SearchExercises/index.jsx
+++ src/component/SearchExercises/index.jsx
@@ -86,9 +86,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
         <HorizontalScrollbar
           data={bodyParts}
-          bodyParts
           setBodyPart={setBodyPart}
           bodyPart={bodyPart}
+          isBodyParts
         />
       </Box>
     </Stack>
