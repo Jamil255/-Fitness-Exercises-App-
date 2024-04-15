b271984d144534b6b6228f18275799318d158906
unknown <jamilafzal255@gmail.com>
2024-04-16 01:45:49 +0500

    complete

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,15 @@ export const exerciseOptions = {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
+ export const youtubeOptions = {
+    method: 'GET',
+    url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+    headers: {
+        'X-RapidAPI-Key': 'ff96d73bd1msh61b58290e887b4dp10e5ecjsn461a025d8b50',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
 
 
 export const fetchData = async (url, options) => {
