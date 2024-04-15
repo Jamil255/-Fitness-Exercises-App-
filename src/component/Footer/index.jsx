7f16ab8deb96edc3734a774253bd73b72ea503cd
unknown <jamilafzal255@gmail.com>
2024-04-16 01:49:07 +0500

    add footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Logo from '/icon/Logo-1.png'
 
-const Footer = () => {
-  return (
-    <>
-      <h4>Footer</h4>
-    </>
-  )
-}
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack
+      gap="40px"
+      sx={{ alignItems: 'center' }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    </Stack>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: '28px', xs: '20px' } }}
+      mt="41px"
+      textAlign="center"
+      pb="40px"
+    >
+      Made with ❤️ by jamil Afzal
+    </Typography>
+  </Box>
+)
 
 export default Footer
